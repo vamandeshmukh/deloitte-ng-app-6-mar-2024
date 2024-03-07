089932dd19b5677b6c122e23910e367f799ffbe6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-07 12:42:33 +0530

    ng basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'deloitte-ng-app';
+}
+
+// @Component({})
 
 // @Component({
 //   selector: 'app-root',
 //   standalone: true,
 //   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
+//   // templateUrl: './app.component.html',
+//   template: `<p>App component works!</p>`
 // })
-
-// @Component({})
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  // templateUrl: './app.component.html',
-  template: `<p>App component works!</p>`
-})
-export class AppComponent {
-
-  title = 'deloitte-ng-app';
-  firstName = 'Sonu';
-
-}
 
 
 
