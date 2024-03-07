a1070ed8983df05710f60138240e13832d20dff1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-07 12:31:49 +0530

    ng basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+
+// @Component({})
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // templateUrl: './app.component.html',
+  template: `<p>App component works!</p>`
 })
 export class AppComponent {
+
   title = 'deloitte-ng-app';
   firstName = 'Sonu';
+
 }
+
+
+
+
+
+
+
 
