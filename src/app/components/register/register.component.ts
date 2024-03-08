9383eaac939d5aaa63dc1ed3dfab326c0fab9c9a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-08 16:44:51 +0530

    ng forms and more

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+// hands on 
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, } from '@angular/forms';
@@ -16,18 +17,59 @@ export class RegisterComponent {
   constructor(private formBuilder: FormBuilder) {
 
     this.registerForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      username: ['',
+        {
+          validators: [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+          updateOn: 'blur'
+        }],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
-    });
+    },
+      { updateOn: 'submit' }
+    );
   }
 
   onSubmit() {
+    console.log('onSubmit');
     if (this.registerForm.valid) {
-      console.log(this.registerForm.value.username);
+      console.log(this.registerForm.value);
       this.registerForm.reset();
     }
   }
 }
+
+
+
+// import { CommonModule } from '@angular/common';
+// import { Component } from '@angular/core';
+// import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-register',
+//   standalone: true,
+//   imports: [ReactiveFormsModule, CommonModule],
+//   templateUrl: './register.component.html',
+//   styleUrl: './register.component.css'
+// })
+// export class RegisterComponent {
+
+//   registerForm: FormGroup;
+
+//   constructor(private formBuilder: FormBuilder) {
+
+//     this.registerForm = this.formBuilder.group({
+//       username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+//       password: ['', [Validators.required]]
+//     });
+//   }
+
+//   onSubmit() {
+//     if (this.registerForm.valid) {
+//       console.log(this.registerForm.value.username);
+//       this.registerForm.reset();
+//     }
+//   }
+// }
 
 
 // import { CommonModule } from '@angular/common';
@@ -60,3 +102,4 @@ export class RegisterComponent {
 //     }
 //   }
 // }
+
