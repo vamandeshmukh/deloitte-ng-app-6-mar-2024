d08148800a798a86c17abda243e5b0701a634c7e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-08 15:03:47 +0530

    ng forms and more

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -16,8 +16,8 @@ export class RegisterComponent {
   constructor(private formBuilder: FormBuilder) {
 
     this.registerForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      password: ['', [Validators.required]]
     });
   }
 
@@ -28,3 +28,35 @@ export class RegisterComponent {
     }
   }
 }
+
+
+// import { CommonModule } from '@angular/common';
+// import { Component } from '@angular/core';
+// import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-register',
+//   standalone: true,
+//   imports: [ReactiveFormsModule, CommonModule],
+//   templateUrl: './register.component.html',
+//   styleUrl: './register.component.css'
+// })
+// export class RegisterComponent {
+
+//   registerForm: FormGroup;
+
+//   constructor(private formBuilder: FormBuilder) {
+
+//     this.registerForm = this.formBuilder.group({
+//       username: ['', Validators.required],
+//       password: ['', Validators.required]
+//     });
+//   }
+
+//   onSubmit() {
+//     if (this.registerForm.valid) {
+//       console.log(this.registerForm.value.username);
+//       this.registerForm.reset();
+//     }
+//   }
+// }
