a6b9a4527c3d35b369b2c3c0805ba7b604530182
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-12 12:52:59 +0530

    routes and more

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,31 +12,63 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailComponent implements OnInit, OnDestroy {
+export class ProductDetailComponent implements OnInit {
 
-  @Input() product: Product | any;
+  product: Product | any;
+  productId: number | any;
 
-  @Output()
-  goBack = new EventEmitter<void>();
-
-  onClick = () => {
-    this.goBack.emit();
-  };
-
-  constructor(private activatedRoute: ActivatedRoute) {
-    console.log('constructor');
-  }
-
-  ngOnChanges() {
-    console.log('ngOnChanges');
+  constructor(private activatedRoute: ActivatedRoute, private productService: ProductService) {
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit');
+    this.productId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.productService.getProductById(this.productId)
+      .subscribe((response) => {
+        console.log(response);
+        this.product = response;
+      }
+      );
   }
-
-  ngOnDestroy() {
-    console.log('ngOnDestroy');
-  }
-
 }
+
+
+
+// import { CommonModule } from '@angular/common';
+// import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+// import { Product } from '../../models/product.model';
+
+// @Component({
+//   selector: 'app-product-details',
+//   standalone: true,
+//   imports: [CommonModule],
+//   templateUrl: './product-details.component.html',
+//   styleUrl: './product-details.component.css'
+// })
+// export class ProductDetailComponent implements OnInit, OnDestroy {
+
+//   @Input() product: Product | any;
+
+//   @Output()
+//   goBack = new EventEmitter<void>();
+
+//   onClick = () => {
+//     this.goBack.emit();
+//   };
+
+//   constructor() {
+//     console.log('constructor');
+//   }
+
+//   ngOnChanges() {
+//     console.log('ngOnChanges');
+//   }
+
+//   ngOnInit(): void {
+//     console.log('ngOnInit');
+//   }
+
+//   ngOnDestroy() {
+//     console.log('ngOnDestroy');
+//   }
+
+// }
