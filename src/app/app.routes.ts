63b8ac139c6a015ddd68899c22af84f7d8665a47
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-12 11:39:24 +0530

    routes and more

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,29 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { Page404Component } from './components/page404/page404.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'products', component: ProductListComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: Page404Component },
 ];
+
+
+
+// import { Routes } from '@angular/router';
+// import { HomeComponent } from './components/home/home.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { RegisterComponent } from './components/register/register.component';
+// import { ProductListComponent } from './components/product-list/product-list.component';
+
+// export const routes: Routes = [
+//     { path: 'home', component: HomeComponent },
+//     { path: 'products', component: ProductListComponent },
+//     { path: 'login', component: LoginComponent },
+//     { path: 'register', component: RegisterComponent },
+// ];
 
