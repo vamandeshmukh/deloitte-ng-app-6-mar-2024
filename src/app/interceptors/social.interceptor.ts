62a1dfc13d05c2b383b9b9523f321262952c91f0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-03-12 10:18:42 +0530

    http and more

--- src/app/interceptors/social.interceptor.ts
+++ src/app/interceptors/social.interceptor.ts
@@ -1,8 +1,20 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { of } from 'rxjs/internal/observable/of';
 
 export const socialInterceptor: HttpInterceptorFn = (request, next) => {
   console.log(request);
   console.log(next);
+  // if (request && (request.url.includes('ghost')))
+  //   throw new Error('Do not search ghosts!');
+  // return next(request);
+
+  if (request && (request.url.includes('ghost'))) {
+    const response = new HttpResponse({
+      status: 422,
+      statusText: 'Do not search ghosts!'
+    });
+    return of(response);
+  }
   return next(request);
 };
 
